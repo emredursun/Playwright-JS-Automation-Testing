42589238fbd9fe00d2abb99fed1546a679ae8736
emre <edursun@sogyo.nl>
2024-03-02 23:57:35 +0100

    Techniques to wait dynamically for new page in Service based applications

---
+++ tests/ClientApp.spec.js
@@ -0,0 +1,25 @@
+const { test, expect } = require("@playwright/test");
+
+test("Browser Context Playwrigth Test", async ({ page }) => {
+
+  const userName= page.locator("#userEmail");
+  const userPassword= page.locator("#userPassword");
+  const loginBtn = page.locator("[value='Login']");
+
+  await page.goto("https://rahulshettyacademy.com/client");
+  await userName.fill("anshika@gmail.com");
+  await userPassword.fill("Iamking@000");
+  await loginBtn.click();
+  await page.waitForLoadState('networkidle');
+
+
+  const adidasCard = page.locator(".card-body [style*='uppercase']").nth(1);
+  const allTitles = page.locator(".card-body b");
+
+  console.log(await adidasCard.textContent());
+  await expect(adidasCard).toContainText("ADIDAS ORIGINAL");
+  console.log(await allTitles.allTextContents());
+
+
+
+});
